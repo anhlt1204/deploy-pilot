73d59693a9e594be852f8828283e629458440084
anhlt1204 <81520604+anhlt1204@users.noreply.github.com>
2021-11-16 17:31:26 +0700

    Update SpringSecurityConfiguration.java

--- src/main/java/com/esdo/bepilot/Config/SpringSecurityConfiguration.java
+++ src/main/java/com/esdo/bepilot/Config/SpringSecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.esdo.bepilot.Util.CustomJwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +15,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
